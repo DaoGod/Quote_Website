df3547a204c45aae1d15639b1ba9b9b9778c27ef
Ammos <Ammos@LAPTOP-AMMOS>
2020-12-02 15:55:16 +0100

    Commit

---
+++ spring-quote/src/test/java/com/ynov/quotes/SpringQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-quotes-service/src/test/java/com/ynov/quotes/SpringQuotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
