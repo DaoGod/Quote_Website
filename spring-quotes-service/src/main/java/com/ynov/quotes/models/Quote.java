a74d968803f01b9bf3a5990fb06d43848bd172d7
Ammos <Ammos@LAPTOP-AMMOS>
2020-12-08 20:15:45 +0100

    Commit Final Projet Quotes_Website

---
+++ spring-quotes-service/src/main/java/com/ynov/quotes/models/Quote.java
@@ -0,0 +1,37 @@
+package com.ynov.quotes.models;
+
+public class Quote {
+	
+	private String quoteID;
+	private String quote;
+	private String author;
+	
+	public Quote() {
+		
+	}
+	
+	public Quote(String quoteID, String quote, String author) {
+		this.quoteID = quoteID;
+		this.quote = quote;
+		this.author = author;
+	}
+	
+	public String getQuoteID() {
+		return quoteID;
+	}
+	public void setQuoteID(String quoteID) {
+		this.quoteID = quoteID;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
